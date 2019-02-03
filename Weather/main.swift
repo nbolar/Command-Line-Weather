1a1a79864736ad35bb500ecbb393d6386bca5d91
Nikhil Bolar <nikhilbolar@nikhils-macbook-pro.local>
2019-02-03 11:00:25 +0300

    Initial Commit

---
+++ Weather/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Weather
+//
+//  Created by Nikhil Bolar on 2/3/19.
+//  Copyright © 2019 Nikhil Bolar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
